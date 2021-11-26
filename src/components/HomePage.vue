91870f15730facaf9449c81da0d2752371da79c5
Jack <jackcoberman@gmail.com>
2021-11-26 12:31:01 -0500

    Works with non-zero values

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -26,7 +26,7 @@
         </v-row>
         <v-spacer/>
         <v-container v-if="hasLengths">
-            <h1>Row length is {{rowLength}} and column length is {{columnLength}}</h1>
+            <!-- <h1>Row length is {{rowLength}} and column length is {{columnLength}}</h1> -->
             <template v-for="(row,r) in originalMatrix">
                 <v-row :key="r">
                     <template v-for="(col, c) in row">
@@ -45,13 +45,25 @@
                 </v-row>
             </template>
         </v-container>
-        <v-btn @click="printMatrix">Print Matrix</v-btn>
-        <v-btn @click="gramSchmidt">Print Gram-Schmidt</v-btn>
+        <v-btn @click="gramSchmidt" v-if="hasLengths">Print Gram-Schmidt</v-btn>
+        <v-container v-if="gramSchmidted">
+            <h3>Result of Gram-Schmidt</h3>
+            <template v-for="(row,r) in Q">
+                <v-row :key="r">
+                    <template v-for="(col, c) in row">
+                        <v-col :key="c" cols="1">
+                            <span>{{Q[r][c].toFixed(3)}}</span>
+                        </v-col>
+                    </template>
+                </v-row>
+            </template>
+        </v-container>
     </v-container>
 </template>
 
 <script>
 import { gramSchmidt } from '@/utils/gramSchmidt';
+import clonedeep from 'lodash.clonedeep';
 
 export default {
     name: 'HomePage',
@@ -60,7 +72,9 @@ export default {
             rowLength: 0,
             columnLength: 0,
             originalMatrix: [],
-            Q: []
+            Q: [],
+            matrix: [],
+            gramSchmidted: false
         };
     },
     methods: {
@@ -72,16 +86,18 @@ export default {
                     this.originalMatrix[r].push(0);
                 }
             }
+            this.gramSchmidted = false;
             // console.log(this.originalMatrix);
         },
-        printMatrix () {
-            console.log(this.originalMatrix);
-        },
         gramSchmidt () {
-            this.Q = gramSchmidt(this.originalMatrix.map(row => row = row.map(
+            this.originalMatrix = this.originalMatrix.map(row => row = row.map(
                 x => x = parseInt(x)
-            )));
-            console.log(this.Q);
+            ));
+            this.matrix = clonedeep(this.originalMatrix);   
+            // console.log(this.matrix);
+            this.Q = gramSchmidt(this.matrix);
+            // console.log(this.Q);
+            this.gramSchmidted = true;
         }
     },
     computed: {
--- src/utils/gramSchmidt.js
+++ src/utils/gramSchmidt.js
@@ -1,4 +1,4 @@
-// import clonedeep from 'lodash.clonedeep';
+import clonedeep from 'lodash.clonedeep';
 
 const dotProduct = (vector1, vector2) => {
     let sum = 0;
@@ -17,38 +17,44 @@ const scaleVector = (scale, vector) => {
     return v;
 }
 
-const addVector = (num, array) => {
-    console.log(num);
-    return array.map(x => x + num);
+const addVectors = (vector1, vector2) => {
+    let vector = [];
+    console.log(vector1);
+    for (let i = 0; i < vector1.length; ++i) {
+        vector.push(vector1[i] + vector2[i]);
+    }
+    return vector;
 }
 export const gramSchmidt = (matrix) => {
     // inspired by the Gram Schmidt Algorithm I had to write in Linear Algebra Lab
     let rows = matrix.length;
     // let cols = matrix[0].length;
-    let Q = matrix;
-
-    // // deep clone the matrix
-    // // for (let r = 0; r < rows; ++r) {
-    // //     Q.push([]);
-    // //     for (let c = 0; c < cols; ++c) {
-    // //         console.log(parseInt(matrix[r][c]));
-    // //         Q[r].push(parseInt(matrix[r][c]));
-    // //     }
-    // // }
-    // console.log(matrix);
-    console.log(Q);
-
-    // Q = matrix.map(x => x = x.map(y => y = parseInt(y)));
-    // console.log(Q);
-
+    let Q = clonedeep(matrix);
 
     // compute orthogonal vectors
     for (let i = 1; i < rows; ++i) {
-        let sum = 0;
+        let vecSum = clonedeep(Q[i].map(val => val = val/val - 1));
+        // console.log(vecSum);
         for (let j = 0; j < i; ++j) {
-            sum += scaleVector((dotProduct(Q[j], Q[i])/dotProduct(Q[j],Q[j])), Q[j]); 
+            vecSum = addVectors(vecSum, scaleVector(-1 * ((dotProduct(Q[j], Q[i])/dotProduct(Q[j],Q[j]))), Q[j])); 
         }
-        Q[i] = addVector(-1 * sum, Q[i]);
+        Q[i] = addVectors(vecSum, Q[i]);
     }
+
+    // drop redundant vectors
+    // for (let i = 0; i < Q.length; ++i) {
+    //     let hasNullity = false;
+    //     for (let j = 0; j < Q[i].length; ++j) {
+    //         if (Number.isNaN(Q[i][j])) {
+    //             hasNullity = true;
+    //             break;
+    //         }
+    //     }
+    //     if (hasNullity) {
+    //         Q.pop(i);
+    //         i--;
+    //     }
+    // }
+    console.log(Q);
     return Q;
 };
