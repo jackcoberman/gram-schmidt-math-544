f99ab36069c4d29da459200f5fc69675a191128a
Jack <jackcoberman@gmail.com>
2021-11-25 19:18:38 -0500

    vuetify set up, basic length request

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!-- <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+        <span class="bar-title">Gram-Schmidt Algorithm</span>
+
+        <!-- <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        /> -->
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">About the Algorithm</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HomePage/>
+      <!-- <HelloWorld/> -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+import HomePage from './components/HomePage';
+
+export default {
+  name: 'App',
+
+  components: {
+    // HelloWorld,
+    HomePage
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+  .bar-title {
+    font-size: 25px;
+  }
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container>
+        <h1 class="size-text">Input Sizes</h1>
+        <v-row>
+            <v-col>
+                <v-text-field
+                    v-model="rowLength"
+                    label="Row Length"
+                    placeholder="0"
+                    clearable
+                    type="number"
+                />
+            </v-col>
+            <v-col>
+                <v-text-field
+                    v-model="columnLength"
+                    label="Column Length"
+                    placeholder="0"
+                    value="0"
+                    clearable
+                    type="number"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data () {
+        return {
+            rowLength: 0,
+            columnLength: 0
+        };
+    },
+    methods: {
+        sizeChange () {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
